ebd0a58e052b5eef7779bb9c34df6f9dd105ff5f
Eric Scouten <scouten@adobe.com>
2024-09-05 10:19:03 -0700

    feat(b): Add `pub fn add6`

--- rp_sandbox_b/src/lib.rs
+++ rp_sandbox_b/src/lib.rs
@@ -1,5 +1,11 @@
+use rp_sandbox_a::add3;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+pub fn add6(n: u64) -> u64 {
+    add3(add3(n))
 }
 
 #[cfg(test)]
@@ -10,5 +16,11 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_add6() {
+        let result = add6(2);
+        assert_eq!(result, 8);
     }
 }
