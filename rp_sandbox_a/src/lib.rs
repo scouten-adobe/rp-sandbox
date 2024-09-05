84453d3c393543e2e082a317ec418e73940b6553
Eric Scouten <scouten@adobe.com>
2024-09-04 21:30:37 -0700

    feat(a): Subtract

--- rp_sandbox_a/src/lib.rs
+++ rp_sandbox_a/src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn subtract(left: i64, right: i64) -> i64 {
+    left - right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +14,11 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn fn_subtract() {
+        let result = subtract(2, -4);
+        assert_eq!(result, -2);
     }
 }
